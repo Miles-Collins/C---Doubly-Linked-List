de782b854d1d7189e7cab20a1412c18410ffc5e9
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2026-09-03 11:26:02 -0700

    added link list required functions, start coding out functionality of insertat head

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -6,6 +6,44 @@
  */
 
 #include "doubly_linked_list.h"
+#include <iostream>
+
+DoublyLinkedList::DoublyLinkedList() {
+  head = nullptr;
+  tail = nullptr;
+}
+
+bool DoublyLinkedList::isEmpty() {
+  return head == nullptr;
+}
+
+void DoublyLinkedList::insertAtHead(int value) {
+  DllNode* newNode = new DllNode(value);
+
+  if (isEmpty()) {
+    head = newNode;
+    tail = newNode;
+  } else {
+    
+  }
+}
+
+void DoublyLinkedList::insertAtTail(int value) {}
+void DoublyLinkedList::remove(int value) {}
+
+void DoublyLinkedList::removeHeaderNode() {}
+
+void DoublyLinkedList::removeTailNode() {}
+
+void DoublyLinkedList::moveNodeToHead(int value) {}
+
+void DoublyLinkedList::moveNodeToTail(int value) {}
+
+void DoublyLinkedList::clear() {}
+
+void DoublyLinkedList::printList() {}
+
+void DoublyLinkedList::reversePrintList() {}
 
 
 
