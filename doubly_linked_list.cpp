b7048b7c455e68d7b929e847653745d1c4efe518
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2026-09-03 10:00:30 -0700

    added doubly_linked_list.cpp and doxygen comments

---
+++ doubly_linked_list.cpp
@@ -0,0 +1,11 @@
+/**
+ * @author Miles Collins
+ * @file doubly_linked_list.cpp
+ * @brief Implementation of a doubly linked list.
+ * @date 9/3/2025 -- Started implementation of the doubly linked list class.
+ */
+
+#include "doubly_linked_list.h"
+
+
+
